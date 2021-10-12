ca58bb116ab6fe279a94979629f9f33b42809fee
HamannMojn <jacob@frost-hamann.dk>
2021-10-12 18:11:05 +0200

    added component and routing for details

--- src/app/credit-card-module/credit-card-details/credit-card-details.component.ts
+++ src/app/credit-card-module/credit-card-details/credit-card-details.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { ActivatedRoute} from '@angular/router';
 import { CreditCard } from 'src/app/creditcard.type';
+import { CreditCardServiceService } from 'src/app/credit-card-module/credit-card-service.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-credit-card-details',
@@ -10,10 +11,28 @@ import { CreditCard } from 'src/app/creditcard.type';
 })
 export class CreditCardDetailsComponent implements OnInit {
 
-  creditcard$: Observable<CreditCard> | null = null;
+  creditcard: CreditCard | undefined;
 
-  constructor(private activatedRoute:ActivatedRoute) { }
+  constructor(
+    private route:ActivatedRoute,
+    private location: Location,
+    private service: CreditCardServiceService
+  ){}
 
-  ngOnInit(): void {}
+  ngOnInit() {
+    this.getCreditCard();
+  }
+
+  getCreditCard(): void {
+    const cardnumber = Number(this.route.snapshot.paramMap.get('id'));
+    this.service.getCreditCard(cardnumber).subscribe(card => this.creditcard = card);
+    console.log(cardnumber);
+    console.log(this.creditcard);
+    
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 
 }
--- src/app/credit-card-module/credit-card-service.service.ts
+++ src/app/credit-card-module/credit-card-service.service.ts
@@ -1,18 +1,29 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpResponse} from '@angular/common/http';
+import { HttpClient} from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { CreditCard } from 'src/app/creditcard.type';
+import { map } from 'rxjs/operators';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class CreditCardServiceService {
-url="http://localhost:3000/credit_cards"
+  url="http://localhost:3000/credit_cards"
+
+  cards: CreditCard[] | undefined;
+  card: CreditCard | undefined;
 
   constructor(private http:HttpClient) { }
 
   getCreditCards() : Observable<CreditCard[]>{
     return this.http.get<CreditCard[]>(this.url);
+  }
+
+  getCreditCard(cardnumber: Number):Observable<CreditCard> {
+    return this.getCreditCards()
+    .pipe(
+      map((cards: any[]) => cards
+      .find((card: { card_number: Number; }) => card.card_number == cardnumber)));
   }
 }
